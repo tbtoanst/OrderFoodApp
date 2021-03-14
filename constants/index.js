bfa866fed3a222b1734aa1d8eeb1e9b84a7b30a1
Toàn Trịnh Bảo <tbtoanst@gmail.com>
2021-03-14 22:44:44 +0700

    Create app

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import images from './images'
+import {COLORS, SIZES, FONTS} from './theme'
+import icons from './icons'
+import GOOGLE_API_KEY from './maps'
+
+export default {images, COLORS, FONTS, SIZES, icons, GOOGLE_API_KEY};
