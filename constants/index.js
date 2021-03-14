3a62d75b8386cdb9fa9043200727b1fd61aac04d
Toàn Trịnh Bảo <tbtoanst@gmail.com>
2021-03-15 00:13:26 +0700

    Add screen folder

--- constants/index.js
+++ constants/index.js
@@ -3,4 +3,4 @@ import {COLORS, SIZES, FONTS} from './theme'
 import icons from './icons'
 import GOOGLE_API_KEY from './maps'
 
-export default {images, COLORS, FONTS, SIZES, icons, GOOGLE_API_KEY};
+export {images, COLORS, FONTS, SIZES, icons, GOOGLE_API_KEY};
---
+++ screens/Restaurant.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Restaurant = () => {
+    <View>
+        <Text>Restaurant</Text>
+    </View>
+}
+
+export default Restaurant;
