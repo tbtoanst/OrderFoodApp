bf2c6d9d1ac3c3c667eb955d9fab3daf026677a8
TOAN <toantb@patc.lan>
2021-03-17 16:54:56 +0700

    add name & description & animation restaurant list

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -9,10 +9,10 @@ import {
   StyleSheet,
   Touchable,
 } from 'react-native';
-
 import {icons, FONTS, SIZES, COLORS} from '../constants';
 
 const Restaurant = ({route, navigation}) => {
+  const scrollX = new Animated.Value(0);
   const [restaurant, setRestaurant] = useState(null);
   const [currentLocation, setCurrentLocation] = useState(null);
 
@@ -89,7 +89,11 @@ const Restaurant = ({route, navigation}) => {
         pagingEnabled
         scrollEventThrottle={16}
         snapToAlignment="center"
-        showsHorizontalScrollIndicator={false}>
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: false},
+        )}>
         {restaurant?.menu.map((item, index) => (
           <View key={`menu-${index}`} style={{alignItems: 'center'}}>
             <View style={{height: SIZES.height * 0.35}}>
@@ -102,6 +106,82 @@ const Restaurant = ({route, navigation}) => {
                   height: '100%',
                 }}
               />
+
+              {/* Quantity */}
+              <View
+                style={{
+                  position: 'absolute',
+                  bottom: -20,
+                  width: SIZES.width,
+                  height: 50,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}>
+                <TouchableOpacity
+                  style={{
+                    width: 50,
+                    justifyContent: 'center',
+                    backgroundColor: COLORS.white,
+                    borderBottomLeftRadius: 25,
+                    borderTopLeftRadius: 25,
+                    alignItems: 'center',
+                  }}>
+                  <Text style={{...FONTS.body1}}>-</Text>
+                </TouchableOpacity>
+                <View
+                  style={{
+                    width: 50,
+                    backgroundColor: COLORS.white,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text style={{...FONTS.h2}}>5</Text>
+                </View>
+                <TouchableOpacity
+                  style={{
+                    width: 50,
+                    justifyContent: 'center',
+                    backgroundColor: COLORS.white,
+                    borderTopRightRadius: 25,
+                    borderBottomRightRadius: 25,
+                    alignItems: 'center',
+                  }}>
+                  <Text style={{...FONTS.body1}}>+</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            {/* Name & Description */}
+            <View
+              style={{
+                width: SIZES.width,
+                alignItems: 'center',
+                marginTop: 15,
+                paddingHorizontal: SIZES.padding * 2,
+              }}>
+              <Text
+                style={{marginVertical: 10, textAlign: 'center', ...FONTS.h2}}>
+                {item.name} - {item.price.toFixed(2)}
+              </Text>
+              <Text style={{...FONTS.body3}}>{item.description}</Text>
+            </View>
+
+            {/* Calories */}
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 10,
+              }}>
+              <Image
+                source={icons.fire}
+                style={{
+                  height: 20,
+                  width: 20,
+                  marginRight: 10,
+                }}
+              />
+              <Text style={{...FONTS.body3, color: COLORS.darkgray}}>
+                {item.calories.toFixed(2)} cal
+              </Text>
             </View>
           </View>
         ))}
@@ -109,10 +189,85 @@ const Restaurant = ({route, navigation}) => {
     );
   }
 
+  function renderDots() {
+    const dotPosition = Animated.divide(scrollX, SIZES.width);
+
+    return (
+      <View style={{height: 30}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: SIZES.padding,
+          }}>
+          {restaurant?.menu.map((item, index) => {
+            const opacity = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+
+            const dotSize = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp',
+            });
+
+            const dotColor = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: 'clamp',
+            });
+
+            return (
+              <Animated.View
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius,
+                  marginHorizontal: 6,
+                  width: dotSize,
+                  height: dotSize,
+                  backgroundColor: dotColor,
+                }}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+
+  function renderOrder() {
+    return (
+      <View>
+        {renderDots()}
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+              borderBottomColor: COLORS.lightGray2,
+              borderBottomWidth: 1,
+            }}></View>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderFoodInfo()}
+      {renderOrder()}
     </SafeAreaView>
   );
 };
